2391cfb97e5ef0f3d9de5a5c3a1a8c46fbe9689d
danieldiazde <daniel.diaz.deleon@outlook.com>
2026-02-19 11:26:29 -0600

    Initial Commit

---
+++ Fledge/FledgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FledgeApp.swift
+//  Fledge
+//
+//  Created by Daniel Diaz de Leon on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FledgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
